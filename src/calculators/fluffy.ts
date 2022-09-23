5f6074dad9fdf4d2e30eb167d7486f5484b62c54
EndormSolor <114251942+EndormSolor@users.noreply.github.com>
2022-09-23 13:17:02 -0400

    Update fluffy.ts
    Updates the level up table to take into account current xp for the level fluffy is currently on, should fix issues with level at zone table.

--- src/calculators/fluffy.ts
+++ src/calculators/fluffy.ts
@@ -332,9 +332,12 @@ export class fluffyInstance {
     const currentRelativeExp = this.evolution === evolution ? this.currentExp : 0;
     // prettier-ignore
     let neededExpForLevel = this.getUpgradeExp(evolution, level, currentRelativeExp);
+    
+    neededExpForLevel = neededExpForLevel - currentRelativeExp
+    
     if (
       this.xpFromZone(zoneLastLeveled, maxZone) >
-      neededExpForLevel - currentRelativeExp
+      neededExpForLevel
     ) {
       for (var z = zone; z < maxZone; z++) {
         let collectedExp = this.xpFromZone(zoneLastLeveled, z + 1);
